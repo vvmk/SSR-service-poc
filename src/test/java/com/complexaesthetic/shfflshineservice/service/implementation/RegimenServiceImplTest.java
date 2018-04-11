eee230cbdad0cbed2ce5493b8f59f9852cc61d4c
vvmk <vincentmasiello@gmail.com>
2018-04-10 22:44:17 -0400

    Create service test files

---
+++ src/test/java/com/complexaesthetic/shfflshineservice/service/implementation/RegimenServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.complexaesthetic.shfflshineservice.service.implementation;
+
+import static org.junit.Assert.*;
+
+/**
+ * project: shffl-shine-service
+ * package: com.complexaesthetic.shfflshineservice.service.implementation
+ * author: https://github.com/vvmk
+ * date: 4/10/18
+ */
+public class RegimenServiceImplTest {
+
+}
