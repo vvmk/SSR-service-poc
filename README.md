2a414e8d94e35289887601c2d67f2fdd4e9e27d0
vvmk <vincentmasiello@gmail.com>
2018-04-10 20:59:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+The backend for ShfflShineRepeat. Handles the persistence of practice routines. Front end is very much in a state of flux while I decide which language I want to use.
---
+++ src/main/java/com/complexaesthetic/shfflshineservice/ShfflShineServiceApplication.java
@@ -0,0 +1,12 @@
+package com.complexaesthetic.shfflshineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShfflShineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShfflShineServiceApplication.class, args);
+	}
+}
