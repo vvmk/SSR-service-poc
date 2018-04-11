7bba3166fe6863235c7dd4a99a56e44df753665e
vvmk <vincentmasiello@gmail.com>
2018-04-10 22:26:46 -0400

    Create service implementations

---
+++ src/main/java/com/complexaesthetic/shfflshineservice/service/implementation/RegimenServiceImpl.java
@@ -0,0 +1,15 @@
+package com.complexaesthetic.shfflshineservice.service.implementation;
+
+import com.complexaesthetic.shfflshineservice.service.interfaces.RegimenService;
+import org.springframework.stereotype.Service;
+
+/**
+ * project: shffl-shine-service
+ * package: com.complexaesthetic.shfflshineservice.service.implementation
+ * author: https://github.com/vvmk
+ * date: 4/10/18
+ */
+
+@Service
+public class RegimenServiceImpl implements RegimenService {
+}
